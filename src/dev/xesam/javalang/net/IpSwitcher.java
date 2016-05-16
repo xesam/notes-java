6dfbf83d45d93b2e75966ab99f5cacb3b28cbe1a
xesam <xesamguo@gmail.com>
2016-05-16 18:38:13 +0800

    IpSwitcher

---
+++ src/dev/xesam/javalang/net/IpSwitcher.java
@@ -0,0 +1,52 @@
+package dev.xesam.javalang.net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by xesamguo@gmail.com on 16-5-16.
+ */
+public class IpSwitcher {
+
+    public static void main(String[] args) {
+        IpSwitcher ipSwitcher = new IpSwitcher("test.chelaile.net.cn");
+        ipSwitcher.overture();
+    }
+
+    private final String domain;
+    private String ip;
+
+    private String used;
+
+    public IpSwitcher(String domain) {
+        this.domain = domain;
+        this.used = domain;
+    }
+
+    public void overture() {
+        try {
+            InetAddress inetAddress = InetAddress.getByName(domain);
+            ip = inetAddress.getHostAddress();
+            used = ip;
+            System.out.println(ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getHost() {
+        return used;
+    }
+
+    public void changeToDomain() {
+        used = domain;
+    }
+
+    public void changeToIp() {
+        if (ip == null || ip.length() == 0) {
+            return;
+        }
+        used = ip;
+    }
+
+}
---
+++ src/dev/xesam/javalang/net/MacTest.java
@@ -0,0 +1,10 @@
+package dev.xesam.javalang.net;
+
+/**
+ * Created by xe on 16-5-16.
+ */
+public class MacTest {
+    public static void main(String[] args) {
+
+    }
+}
