bf3a635c394ad41feef271d330e33453907f6afb
xesam <xesamguo@gmail.com>
2016-05-31 11:11:18 +0800

    cas

---
+++ src/dev/xesam/javalang/concurrent/CAS.java
@@ -0,0 +1,14 @@
+package dev.xesam.javalang.concurrent;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by xe on 16-5-31.
+ */
+public class CAS {
+    public static void main(String[] args) {
+        AtomicBoolean locked = new AtomicBoolean(false);
+        System.out.println(locked.compareAndSet(false, true));
+        System.out.println(locked.get());
+    }
+}
