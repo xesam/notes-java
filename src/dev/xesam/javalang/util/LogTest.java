e385ace9b66a21a9338e0577726e09a8c3f3b170
xesam <xesamguo@gmail.com>
2016-06-03 12:11:31 +0800

    LogTest

--- src/dev/xesam/javalang/util/LogTest.java
+++ src/dev/xesam/javalang/util/LogTest.java
@@ -8,8 +8,8 @@ import java.util.stream.IntStream;
 /**
  * Created by xe on 16-6-3.
  */
-public class Main {
-    static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+public class LogTest {
+    static Logger logger = LoggerFactory.getLogger(LogTest.class.getName());
 
     public static void main(String[] args) {
         IntStream.range(0, 10).forEach(i -> logger.debug(i + ""));
