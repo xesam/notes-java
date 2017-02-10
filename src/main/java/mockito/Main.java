a7860ea7d2d655127388f6c1e02a46815fe4ea45
xesam <xesamguo@gmail.com>
2017-02-10 18:39:46 +0800

    mockito

---
+++ src/main/java/mockito/Main.java
@@ -0,0 +1,12 @@
+package mockito;
+
+import org.mockito.Mockito;
+
+/**
+ * Created by xesamguo@gmail.com on 17-1-20.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        Mockito
+    }
+}
