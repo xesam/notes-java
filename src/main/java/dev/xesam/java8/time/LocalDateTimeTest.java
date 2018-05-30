bf8c6e4c4bf5d470429f3cdde5670daaf55c94a4
xesam <xesamguo@gmail.com>
2018-05-30 18:27:39 +0800

    DateTime

---
+++ src/main/java/dev/xesam/java8/time/LocalDateTimeTest.java
@@ -0,0 +1,7 @@
+package dev.xesam.java8.time;
+
+public class LocalDateTimeTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/dev/xesam/java8/time/LocalTimeTest.java
@@ -0,0 +1,17 @@
+package dev.xesam.java8.time;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalTimeTest {
+    public static void main(String[] args) {
+        LocalTime time1 = LocalTime.parse("12:00");
+        System.out.println(time1.getHour());
+        LocalTime time2 = LocalTime.parse("12/00", DateTimeFormatter.ofPattern("HH/MM"));
+        System.out.println(time2.getHour());
+        LocalTime time3 = LocalTime.parse("12 00", DateTimeFormatter.ofPattern("HH MM"));
+        System.out.println(time3.getHour());
+        LocalTime time4 = LocalTime.parse("12：00", DateTimeFormatter.ofPattern("HH：MM"));
+        System.out.println(time4.getHour());
+    }
+}
