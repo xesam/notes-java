dcd47cebed4f4f6ca6aaecce2d81455909a99a7b
xesam <xesamguo@gmail.com>
2018-05-31 10:59:35 +0800

    Stream

--- src/main/java/dev/xesam/java8/stream/StreamTest1.java
+++ src/main/java/dev/xesam/java8/stream/StreamTest1.java
@@ -3,6 +3,8 @@ package dev.xesam.java8.stream;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -17,6 +19,12 @@ public class StreamTest1 {
                 .filter(x -> x > 2)
                 .count();
         assertEquals(2, a);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        List<String> vals = Arrays.stream(arr).mapToObj(value -> "str_" + value).collect(Collectors.toList());
+        System.out.println(vals);
     }
 
 }
