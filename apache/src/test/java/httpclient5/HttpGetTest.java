0aaf9598fd9ea9273437cf46cb748c258d9dd9de
xesam <xesamguo@gmail.com>
2022-09-13 16:37:05 +0800

    mock /fix

--- apache/src/test/java/httpclient5/HttpGetTest.java
+++ apache/src/test/java/httpclient5/HttpGetTest.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
@@ -41,14 +42,7 @@ public class HttpGetTest {
 
     @Test
     public void testPost() throws IOException {
-        HttpPost request = new HttpPost("http://httpbin.org/1");
-        request.addHeader("header1", "value1");
-        List<NameValuePair> data = new ArrayList<>();
-        data.add(new BasicNameValuePair("name", "value"));
-        data.add(new BasicNameValuePair("name2", "value2"));
-        request.setEntity(new UrlEncodedFormEntity(data));
-
-        HttpClient httpClient = spy(HttpClient.class);
+        CloseableHttpClient httpClient = mock(CloseableHttpClient.class);
         when(httpClient.execute(any())).thenAnswer((Answer<HttpResponse>) invocationOnMock -> {
             Object[] args = invocationOnMock.getArguments();
             HttpPost argRequest = (HttpPost) args[0];
@@ -58,6 +52,12 @@ public class HttpGetTest {
             Assert.assertEquals("value1", argRequest.getHeader("header1").getValue());
             return null;
         });
+        HttpPost request = new HttpPost("http://httpbin.org/1");
+        request.addHeader("header1", "value1");
+        List<NameValuePair> data = new ArrayList<>();
+        data.add(new BasicNameValuePair("name", "value"));
+        data.add(new BasicNameValuePair("name2", "value2"));
+        request.setEntity(new UrlEncodedFormEntity(data));
         httpClient.execute(request);
     }
 }
